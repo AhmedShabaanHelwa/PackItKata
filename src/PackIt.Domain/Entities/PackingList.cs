80ef0ca32c33792de6de06b4f43be9df330ad013
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-10 14:05:29 +0200

    PackIt.Domain: Add PackingList entity class.

---
+++ src/PackIt.Domain/Entities/PackingList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PackIt.Domain.Entities
+{
+    /// <summary>
+    /// List of the stuff to be packed
+    /// </summary>
+    internal class PackingList
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Localization { get; private set; }
+
+        public PackingList(string name, string localization)
+        => (this.Name, this.Localization) = (name, localization);
+    }
+}
