daa8d434c7ae6267be45c55cc0d864894a638c4a
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-11 16:29:45 +0200

    PackingIt.Domain: Add EmptyPackingListName exception.

---
+++ src/PackIt.Domain/Exceptions/EmptyPackingListNameException.cs
@@ -0,0 +1,12 @@
+﻿using PackIt.Shared.Abstractions.Exceptions;
+
+namespace PackIt.Domain.Exceptions
+{
+    public class EmptyPackingListNameException : PackItExceptionBase
+    {
+        public EmptyPackingListNameException() : base("Packing list name cannot be null or empty.")
+        {
+
+        }
+    }
+}
