010f26bc45114d5a904c10f3d70eaffe510535d3
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-11 16:28:32 +0200

    PackIt.Domain: Add PackItName value object.

---
+++ src/PackIt.Domain/ValueObjects/PackingListName.cs
@@ -0,0 +1,30 @@
+﻿using PackIt.Domain.Exceptions;
+using System.Runtime.CompilerServices;
+
+namespace PackIt.Domain.ValueObjects;
+
+/// <summary>
+/// List name value object.
+/// </summary>
+public record PackingListName
+{
+    public string Value;
+    public PackingListName(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new EmptyPackingListNameException();
+        
+        Value = value;
+    }
+
+    /// <summary>
+    /// Implicit conversion from string to PackingListName.
+    /// </summary>
+    /// <param name="name">Name of the packing list</param>
+    public static implicit operator PackingListName(string name) => new PackingListName(name);
+    /// <summary>
+    /// Implicit conversion from PackingListName to string.
+    /// </summary>
+    /// <param name="name"></param>
+    public static implicit operator string(PackingListName name) => name.Value;
+}
