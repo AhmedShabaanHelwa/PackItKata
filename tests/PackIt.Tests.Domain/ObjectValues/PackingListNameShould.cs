a2f530cda888e23c010f1795d034ca4dc93fb655
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-11 16:59:18 +0200

    PackIt.Tests.Domain: Add PackingList and PackingListName test classes and cases.

---
+++ tests/PackIt.Tests.Domain/ObjectValues/PackingListNameShould.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using PackIt.Domain.ValueObjects;
+using Xunit;
+
+namespace PackIt.Tests.Domain.ObjectValues
+{
+    public class PackingListNameShould
+    {
+        [Fact]
+        internal void ImplicitlyConvertFromStringToPackingListName()
+        {
+            // Arrange
+            string stringfiedName = "Ahmed";
+            // Act
+            var valueObjectName = (PackingListName)stringfiedName;
+            // Assert
+            valueObjectName.Should().BeOfType(typeof(PackingListName));
+            valueObjectName.Value.Should().Be("Ahmed");
+        }
+
+        [Fact]
+        internal void ImplicitlyConvertFromPackingListNameToString()
+        {
+            // Arrange
+            PackingListName objectValueName = new("Ahmed");
+            // Act
+            var stringfiedName = (string)objectValueName;
+            // Assert
+            stringfiedName.Should().BeOfType(typeof(string));
+            stringfiedName.Should().Be("Ahmed");
+        }
+    }
+}
