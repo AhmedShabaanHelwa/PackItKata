2aa6cfe9cc4eddfc06adb52b1de044f9401e63e0
ahmedshabaanhelwa <ahmedshabaan@outlook.com>
2022-05-11 14:32:56 +0200

    PackIt.Shared.Abstractions: Add PackItExceptionBase class.

---
+++ src/Shared/PackIt.Shared.Abstractions/Exceptions/PackItExceptionBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIt.Shared.Abstractions.Exceptions
+{
+    /// <summary>
+    /// PackIt exception base class
+    /// </summary>
+    public abstract class PackItExceptionBase : Exception
+    {
+        protected PackItExceptionBase(string message) : base (message)
+        { 
+        }
+    }
+}
